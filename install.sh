7da6f0c2d3bdc3e164807d18f17e0b5b44b25ce6
François Roland <fr.roland@gmail.com>
2018-09-07 12:28:51 +0200

    Install startup script

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum install -y gcc-c++ make nodejs git
 npm install node-red aws-sdk node-red-contrib-storage-s3 node-red-contrib-mapper node-red-contrib-ttn node-red-dashboard when 
+
+sudo cp node-red.service /etc/systemd/system/node-red.service
+sudo systemctl enable node-red.service
+sudo systemctl start node-red.service
+sudo systemctl status node-red.service
